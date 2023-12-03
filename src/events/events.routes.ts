3a919945ccbc226ad0a491ff783e132475ce4cee
sai shetty <saikumar.shetty@jda.com>
2023-12-03 16:00:23 +0530

    okay ish

--- src/events/events.routes.ts
+++ src/events/events.routes.ts
@@ -21,11 +21,11 @@ class EventRoutes {
             message: 'This endpoint has a stricter rate limiting of a maximum of 200 requests per 15 minutes window, please lower your request rate'
         })
 
-        this.router.post('/', noCache, strictRateLimit, this.eventController.createEvent);
-        this.router.post('/schedule', noCache, strictRateLimit, this.eventController.schedulePaidEvents);
-        this.router.post('/createMultiple', noCache, strictRateLimit, this.eventController.createMultiple);
-        this.router.get('/', noCache, strictRateLimit, this.eventController.getAllEvents);
-        this.router.get('/:id', noCache, strictRateLimit, this.eventController.getEventById);
+        this.router.post('/', noCache, strictRateLimit, (req, res, next) => this.eventController.createEvent(req, res, next));
+        this.router.post('/schedule', noCache, strictRateLimit, (req, res, next) => this.eventController.schedulePaidEvents(req, res, next));
+        this.router.post('/createMultiple', noCache, strictRateLimit, (req, res, next) => this.eventController.createMultiple(req, res, next));
+        this.router.get('/', noCache, strictRateLimit, (req, res, next) => this.eventController.getAllEvents(req, res, next));
+        this.router.get('/:id', noCache, strictRateLimit, (req, res, next) => this.eventController.getEventById(req, res, next));
     }
 }
 
